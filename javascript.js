0919e23f81e1b9dd76279b6f4e5d99b23b51aada
Janishi <106867667+Janishi@users.noreply.github.com>
2022-07-02 03:27:31 +0400

    create javascript.js

---
+++ javascript.js
@@ -0,0 +1,44 @@
+
+// 1.დავალება
+
+let array = ['watermelon','pear',10,45,50,'apple', 'ananas'];
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[5]);
+console.log(array[6]);
+
+// 2.დავალება
+let array2 = [32,10,'hello',null,'hello2',50];
+console.log(array2[1], array2[5]);
+console.log(array2[1]);
+console.log(array2[5]);
+
+// 3. დავალება
+
+// let numbers = [5,6,10,25,3,7,4,100,28,36,89];
+
+// for (let x = 0; x > numbers.length; x++) {
+//     console.log(numbers[x]);
+// }
+
+// 4. დავალება
+
+let user={
+    firstname: 'giorgi',
+    lastname: 'smith',
+    age: 25,
+    studentstatus: active
+}
+console.log(user.studentstatus);
+
+// 5.დავალება
+
+let nameList=['ana','nana', 'dato', 'nino','zura', 'giorgi'];
+
+for (let x = 0;  x  < nameList.length; x ++) {
+    console.log(nameList[x]);
+}
+
+// 6.დავალება
+
+
